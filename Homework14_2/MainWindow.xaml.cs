bd95526b31e46414215c605e4379af27052c3846
kostenkoi24 <kostenko.i@gmail.com>
2023-02-27 20:30:32 +0200

    Homework14

--- Homework14_2/MainWindow.xaml.cs
+++ Homework14_2/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +21,69 @@ namespace Homework14_2
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private static CancellationTokenSource cancelToken = new CancellationTokenSource();
+        private static  CancellationToken token = cancelToken.Token;
+
         public MainWindow()
         {
             InitializeComponent();
+
         }
+
+        void ChangeTextBox(string value)
+        {
+            Dispatcher.BeginInvoke(new Action(() => { TextBox.Text = value; }));
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            ConnectToDB();
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            cancelToken.Cancel();
+            ChangeTextBox("Disconnected to DB");
+        }
+
+        
+        async void ConnectToDB()
+        {
+            try
+            {
+                await Task.Run(() =>
+                {
+                    int x = 10;
+                    for (int i = 1; i <= x; i++)
+                    {
+                        ChangeTextBox("Loading data..." + new string('.', i) + i.ToString() + "from " + x.ToString());
+                        Thread.Sleep(4000);
+
+                        if (token.IsCancellationRequested)
+                        {
+                            return;
+                        }
+                    }
+                }
+            );
+            }
+            catch (OperationCanceledException ex)
+            {
+                ChangeTextBox("Error");
+            }
+            finally
+            {
+                cancelToken.Dispose();
+            }
+
+        }
+
+
+       
+
+
+
+
     }
 }
---
+++ Homework14_4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Homework14_4
+{
+    class Program
+    {
+        static int counter = 0;
+
+        static object block = 0;
+
+        static void Function()
+        {
+            lock (block)
+            {
+                for (int i = 0; i < 5; ++i)
+                {
+                    Console.WriteLine(++counter);
+                    Console.WriteLine("\t{0}", Thread.CurrentThread.GetHashCode());
+                    Thread.Sleep(1000);
+                }
+            }
+
+        }
+
+
+        
+
+
+        static async Task Main()
+        {
+            
+            Task[] task = new Task[3];
+            for (int i = 0; i < 3; i++)
+            {
+                task[i] = new Task(Function);
+                task[i].Start();
+                await task[i];
+            }
+
+            // Delay
+            Console.WriteLine("End");
+            Console.ReadKey();
+        }
+    }
+}
